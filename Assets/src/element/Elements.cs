443c53ed52bdb5ce73c731a48111a206dab98003
Manuel Schmidbauer <manuel2258@protonmail.com>
2019-11-22 15:34:55 +0100

    Optimized ColliderBodyFiltering

--- Assets/src/element/Elements.cs
+++ Assets/src/element/Elements.cs
@@ -16,16 +16,40 @@ namespace src.element {
         }
 
         /// <summary>
-        /// Filters a Stream of Collider2D for ColliderBodys with the right color
+        /// Filters a Array of Collider2D for ColliderBodys with the right color
         /// </summary>
         /// <param name="colliders">The to filter Collider2Ds</param>
         /// <param name="color">The to filter for ElementColor</param>
-        /// <returns>The Filter ColliderBodySteam</returns>
-        public static IEnumerable<ColliderBody> filterForColorFromColliders(IEnumerable<Collider2D> colliders, ElementColor color) {
-            return colliders.Where(collider => collider != null)
-                .Select(collider => collider.GetComponent<ColliderBody>())
-                .Where(colliderBody => colliderBody != null)
-                .Where(colliderBody => colliderBody.Color == color);
+        /// <returns>The Filter ColliderBody Array</returns>
+        public static ColliderBody[] filterForColorFromColliders(Collider2D[] colliders, ElementColor color) {
+            // Bitmask of to return ColliderBodys
+            int mask = 0;
+            int counter = 0;
+            
+            // Array of all casted ColliderBodys
+            var allColliderBodys = new ColliderBody[colliders.Length];
+            for (var i = 0; i < colliders.Length; i++) {
+                var currentCollider = colliders[i];
+                var colliderBody = currentCollider.GetComponent<ColliderBody>();
+                allColliderBodys[i] = colliderBody;
+                if(colliderBody == null) continue;
+                if(colliderBody.Color != color) continue;
+                counter++;
+                
+                // If colliderbody has right color enable it bits at position i
+                mask |= 1 << i;
+            }
+
+            var returnColliders = new ColliderBody[counter];
+            counter = 0;
+            for (var i = 0; i < colliders.Length; i++) {
+                // Check if bit is enabled
+                if ((mask & 1 << i) == 0) continue;
+                returnColliders[counter] = allColliderBodys[i];
+                counter++;
+            }
+
+            return returnColliders;
         }
         
         /// <summary>
